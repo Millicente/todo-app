ac66d1f352842c8875546658bd6f14c615f02210
Millicente <ambrosemillicent10@gmail.com>
2026-02-16 06:32:45 -0500

    Initial commit Django Todo App with add, toggle, delete features

---
+++ tasks/views.py
@@ -0,0 +1,34 @@
+# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Task
+from .forms import TaskForm
+
+@login_required
+def task_list(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            return redirect('tasks:task_list')
+    else:
+        form = TaskForm()
+    
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, 'tasks/task_list.html', {'tasks': tasks, 'form': form})
+
+@login_required
+def toggle_task(request, task_id):
+    task = Task.objects.get(id=task_id, user=request.user)
+    task.completed = not task.completed
+    task.save()
+    return redirect('tasks:task_list')    
+
+@login_required
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id, user=request.user)
+    task.delete()
+    return redirect('tasks:task_list')
+    
